c7e194ad1b01ce70d9e46813df8b4f0673acfd52
AndrewNalley <104536377+AndrewNalley@users.noreply.github.com>
2024-01-08 08:07:36 -0600

    update html description

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,13 @@
 import { NavLink } from "react-router-dom"
-import dog from "../assets/photos/stock-photos/dog-stock.jpg"
+import logo from "../assets/photos/stock-photos/dog-stock.jpg"
 // import logo from './../images/clarion-horn.png'
 
 export default function Header() {
     return (
         <>
             <div className="grid">
-                <h2> Morton's Mementos</h2>
-                <img className="main-photo" src={dog} alt="dog" height={100} width={100} />
+                <h2>HEADER</h2>
+                <img className="main-photo" src={logo} alt="dog" height={100} width={100} />
             </div>
             <nav>
                 <NavLink to='/'>Home</NavLink>
