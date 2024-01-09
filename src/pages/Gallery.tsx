4b474c139b37b49389d5012b5cab031b630f23c1
Andrew Nalley <awnalley@gmail.com>
2024-01-08 20:59:47 -0600

    additional photos, exifr library works with current implementation, added

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,13 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import FillerText from "../components/FillerText";
 import { Gallery } from "react-grid-gallery";
 import { photoArray } from "../assets/PhotoDump";
 
-
 export default function PhotoGallery() {
+    const [loading, setLoading] = useState(true);
+
+  
+
+    useEffect(() => {
+        // Check if the array is filled
+        if (photoArray.length > 0) {
+            setLoading(false); // Set loading to false if the array is filled
+        }
+    }, []); // Empty dependency array to run the effect only once
+
     return (
         <>
             <h1>Gallery of past projects </h1>
-            <Gallery images={photoArray} />
+            
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <Gallery images={photoArray} />
+            )}
             <FillerText />
         </>
     );
