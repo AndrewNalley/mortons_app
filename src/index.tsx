287e90b9c58838364a3b31092d86427615234677
Andrew Nalley <awnalley@gmail.com>
2024-01-10 21:00:05 -0600

    remove react.strictmode while developing gallery, causes components to mount twice. create customPhoto class by extending Image class from react-grid-gallery

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+)
 
+root.render(
+  <App />
+)
+
+// Previously code was as shown below,
+// however, to fix a small react modal bug that occurs only 
+// in development, the above code is implemented.
+// Strict mode will be reinstated after the Gallery is 
+// suitably developed.
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// (see https://stackoverflow.com/questions/60618844/react-hooks-useeffect-is-called-twice-even-if-an-empty-array-is-used-as-an-ar/60619061#60619061)
