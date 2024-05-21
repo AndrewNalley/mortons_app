c2eeb4327909575f06f9ac1d9c9bef56e5922f70
Andrew Nalley <awnalley@gmail.com>
2024-05-21 18:08:46 -0500

    add photos to project, imported about first 50

--- src/assets/PhotoDump.tsx
+++ src/assets/PhotoDump.tsx
@@ -63,6 +63,54 @@ import photo61 from '../assets/photos/proj-photos/Craft_Lindsey-61.jpg'
 import photo62 from '../assets/photos/proj-photos/Craft_Lindsey-62.jpg'
 import photo63 from '../assets/photos/proj-photos/Craft_Lindsey-63.jpg'
 import photo64 from '../assets/photos/proj-photos/Craft_Lindsey-64.jpg'
+// new photos below as of 5/21/2024
+import photo65 from '../assets/photos/proj-photos/DSC_0786.jpg'
+import photo66 from '../assets/photos/proj-photos/DSC_0787.jpg'
+import photo67 from '../assets/photos/proj-photos/DSC_0788.jpg'
+import photo68 from '../assets/photos/proj-photos/DSC_0789.jpg'
+import photo69 from '../assets/photos/proj-photos/DSC_0790.jpg'
+import photo70 from '../assets/photos/proj-photos/DSC_0791.jpg'
+import photo71 from '../assets/photos/proj-photos/DSC_0792.jpg'
+import photo72 from '../assets/photos/proj-photos/DSC_0793.jpg'
+import photo73 from '../assets/photos/proj-photos/DSC_0794.jpg'
+import photo74 from '../assets/photos/proj-photos/DSC_0795.jpg'
+import photo75 from '../assets/photos/proj-photos/DSC_0796.jpg'
+import photo76 from '../assets/photos/proj-photos/DSC_0797.jpg'
+import photo77 from '../assets/photos/proj-photos/DSC_0798.jpg'
+import photo78 from '../assets/photos/proj-photos/DSC_0799.jpg'
+import photo79 from '../assets/photos/proj-photos/DSC_0800.jpg'
+import photo80 from '../assets/photos/proj-photos/DSC_0801.jpg'
+import photo81 from '../assets/photos/proj-photos/DSC_0802.jpg'
+import photo82 from '../assets/photos/proj-photos/DSC_0803.jpg'
+import photo83 from '../assets/photos/proj-photos/DSC_0804.jpg'
+import photo84 from '../assets/photos/proj-photos/DSC_0805.jpg'
+import photo85 from '../assets/photos/proj-photos/DSC_0806.jpg'
+import photo86 from '../assets/photos/proj-photos/DSC_0807.jpg'
+import photo87 from '../assets/photos/proj-photos/DSC_0808.jpg'
+import photo88 from '../assets/photos/proj-photos/DSC_0809.jpg'
+import photo89 from '../assets/photos/proj-photos/DSC_0810.jpg'
+import photo90 from '../assets/photos/proj-photos/DSC_0811.jpg'
+import photo91 from '../assets/photos/proj-photos/DSC_0812.jpg'
+import photo92 from '../assets/photos/proj-photos/DSC_0813.jpg'
+import photo93 from '../assets/photos/proj-photos/DSC_0814.jpg'
+import photo94 from '../assets/photos/proj-photos/DSC_0815.jpg'
+import photo95 from '../assets/photos/proj-photos/DSC_0816.jpg'
+import photo96 from '../assets/photos/proj-photos/DSC_0817.jpg'
+import photo97 from '../assets/photos/proj-photos/DSC_0818.jpg'
+import photo98 from '../assets/photos/proj-photos/DSC_0819.jpg'
+import photo99 from '../assets/photos/proj-photos/DSC_0820.jpg'
+import photo100 from '../assets/photos/proj-photos/DSC_0821.jpg'
+import photo101 from '../assets/photos/proj-photos/DSC_0821.jpg'
+import photo102 from '../assets/photos/proj-photos/DSC_0822.jpg'
+import photo103 from '../assets/photos/proj-photos/DSC_0823.jpg'
+import photo104 from '../assets/photos/proj-photos/DSC_0824.jpg'
+import photo105 from '../assets/photos/proj-photos/DSC_0825.jpg'
+import photo106 from '../assets/photos/proj-photos/DSC_0826.jpg'
+import photo107 from '../assets/photos/proj-photos/DSC_0827.jpg'
+import photo108 from '../assets/photos/proj-photos/DSC_0828.jpg'
+
+
+
 
 const popularArray = [
   { id: 5, title: 'Coaster holder - with flowers and gold foil', src: photo5, width: 1920, height: 1080 },
@@ -225,7 +273,52 @@ const photosArray = [
   { id: 61, title: 'Custom Geode Coasters - purple and white with gold foil', src: photo61, width: 1920, height: 1080 },
   { id: 62, title: 'Custom Geode Coasters - purple and white with gold foil', src: photo62, width: 1920, height: 1080 },
   { id: 63, title: 'Monstera Leaf - White', src: photo63, width: 1920, height: 1080 },
-  { id: 64, title: 'Geode Coasters - Purple and pink with silver foil', src: photo64, width: 1920, height: 1080 }
+  { id: 64, title: 'Geode Coasters - Purple and pink with silver foil', src: photo64, width: 1920, height: 1080 },
+  { id: 65, title: '65', src: photo65, width: 1920, height: 1080 },
+  { id: 66, title: '66', src: photo66, width: 1920, height: 1080 },
+  { id: 67, title: '67', src: photo67, width: 1920, height: 1080 },
+  { id: 68, title: '68', src: photo68, width: 1920, height: 1080 },
+  { id: 69, title: '69', src: photo69, width: 1920, height: 1080 },
+  { id: 70, title: '70', src: photo70, width: 1920, height: 1080 },
+  { id: 71, title: '71', src: photo71, width: 1920, height: 1080 },
+  { id: 72, title: '72', src: photo72, width: 1920, height: 1080 },
+  { id: 73, title: '73', src: photo73, width: 1920, height: 1080 },
+  { id: 74, title: '74', src: photo74, width: 1920, height: 1080 },
+  { id: 75, title: '75', src: photo75, width: 1920, height: 1080 },
+  { id: 76, title: '76', src: photo76, width: 1920, height: 1080 },
+  { id: 77, title: '77', src: photo77, width: 1920, height: 1080 },
+  { id: 78, title: '78', src: photo78, width: 1920, height: 1080 },
+  { id: 79, title: '79', src: photo79, width: 1920, height: 1080 },
+  { id: 80, title: '80', src: photo80, width: 1920, height: 1080 },
+  { id: 81, title: '81', src: photo81, width: 1920, height: 1080 },
+  { id: 82, title: '82', src: photo82, width: 1920, height: 1080 },
+  { id: 83, title: '83', src: photo83, width: 1920, height: 1080 },
+  { id: 84, title: '84', src: photo84, width: 1920, height: 1080 },
+  { id: 85, title: '85', src: photo85, width: 1920, height: 1080 },
+  { id: 86, title: '86', src: photo86, width: 1920, height: 1080 },
+  { id: 87, title: '87', src: photo87, width: 1920, height: 1080 },
+  { id: 88, title: '88', src: photo88, width: 1920, height: 1080 },
+  { id: 89, title: '89', src: photo89, width: 1920, height: 1080 },
+  { id: 90, title: '90', src: photo90, width: 1920, height: 1080 },
+  { id: 91, title: '91', src: photo91, width: 1920, height: 1080 },
+  { id: 92, title: '92', src: photo92, width: 1920, height: 1080 },
+  { id: 93, title: '93', src: photo93, width: 1920, height: 1080 },
+  { id: 94, title: '94', src: photo94, width: 1920, height: 1080 },
+  { id: 95, title: '95', src: photo95, width: 1920, height: 1080 },
+  { id: 96, title: '96', src: photo96, width: 1920, height: 1080 },
+  { id: 97, title: '97', src: photo97, width: 1920, height: 1080 },
+  { id: 98, title: '98', src: photo98, width: 1920, height: 1080 },
+  { id: 99, title: '99', src: photo99, width: 1920, height: 1080 },
+  { id: 100, title: '100', src: photo100, width: 1920, height: 1080 },
+  { id: 101, title: '101', src: photo101, width: 1920, height: 1080 },
+  { id: 102, title: '102', src: photo102, width: 1920, height: 1080 },
+  { id: 103, title: '103', src: photo103, width: 1920, height: 1080 },
+  { id: 104, title: '104', src: photo104, width: 1920, height: 1080 },
+  { id: 105, title: '105', src: photo105, width: 1920, height: 1080 },
+  { id: 106, title: '106', src: photo106, width: 1920, height: 1080 },
+  { id: 107, title: '107', src: photo107, width: 1920, height: 1080 },
+  { id: 108, title: '108', src: photo108, width: 1920, height: 1080 },
+
 ]
 
 
